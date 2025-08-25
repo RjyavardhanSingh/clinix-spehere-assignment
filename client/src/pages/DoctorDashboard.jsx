b76d6ddaf7f08ace04193afdad47cdb110daf805
RajyavardhanSingh <rajyavardhansing2003@gmail.com>
2025-08-25 17:55:30 +0530

    refactor: remove unused CSS import from DoctorDashboard component

--- client/src/pages/DoctorDashboard.jsx
+++ client/src/pages/DoctorDashboard.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
-import "./DoctorDashboard.css";
 import { API } from "../config/api"; // Import the API config
 
 const DoctorDashboard = () => {
